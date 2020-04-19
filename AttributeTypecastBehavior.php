7cb3d532bbafd8639706ff0deaf8f87d9f3b516f
denis909 <denis909@mail.ru>
2020-04-19 13:52:31 +0300

    n/a

--- AttributeTypecastBehavior.php
+++ AttributeTypecastBehavior.php
@@ -2,6 +2,10 @@
 
 namespace denis909\yii;
 
+use Yii;
+use DateTime;
+use DateTimeZone;
+
 class AttributeTypecastBehavior extends \yii\behaviors\AttributeTypecastBehavior
 {
 
@@ -14,7 +18,12 @@ class AttributeTypecastBehavior extends \yii\behaviors\AttributeTypecastBehavior
     {
         if (is_scalar($type) && ($type == static::TYPE_UNIX_TIMESTAMP))
         {
-            return ($value && !is_numeric($value)) ? strtotime($value): $value;   
+            if ($value && !is_numeric($value))
+            {
+                return (new DateTime($value, new DateTimeZone(Yii::$app->formatter->timeZone)))->format('U');
+            }
+
+            return $value; 
         }
 
         return parent::typecastValue($value, $type);
